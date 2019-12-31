cbfa734669a0a73df24080041e8f9740f48484fa
Mohammad Abbasi <M.Abbasi@Pajuheshco.com>
2019-12-31 15:57:35 +0330

    login

--- WalletPan.Web/ClientApps/wallet/src/Pages/LoginPage.js
+++ WalletPan.Web/ClientApps/wallet/src/Pages/LoginPage.js
@@ -12,7 +12,7 @@ const LoginPage = ({ classes }) => {
     return (
         <div className="container-fluid">
 
-            <div className="row login twin-section" >
+            <div className="row login twin-section vh-90 vw-90" >
                 <div className="col-12 col-md-5 left-part">
 
 
@@ -43,7 +43,7 @@ const LoginPage = ({ classes }) => {
                     </div>
                     <div className="row">
                                 <div className="col-12">
-                                    <Form className="custom-form">
+                                    <Form className="custom-form center">
                                         <FormGroup className="form-group" controlId="formBasicEmail">
                                             <FormLabel className="form-label">Email address</FormLabel>
                                             <FormControl  className="form-input"  required type="email" placeholder="Enter your Email address" />
@@ -57,32 +57,35 @@ const LoginPage = ({ classes }) => {
                                             <FormControl  className="form-input"  type="password" placeholder="Enter your password" />
                                         </FormGroup>
 
-                                        <div className="col-12">
+                                        <div className="col-12 mb-3">
                                             <a href="#" >Forget Password?</a>
                                         </div>
                                       
-                                        <Button className="btn-primary" variant="primary" type="submit">
-                                            Submit
+                                        <Button variant="primary" type="submit">
+                                            Login
                                           </Button>
                                          
                                           <Divider title="Or" />
                                               
                                           <Button variant="outline-primary"> <img src={`${process.env.PUBLIC_URL}/landing_assets/google-icon.svg`} /> Sign in with Google</Button>
                                         
-                                          <div className="col-12 form-footer text-center">
-                                          New User? <a href="#" >Register</a>
-                                         </div>
+                                       
                                     </Form>
                                 </div>
                             </div>
                 </div>
                 <div className="col-12 col-md-7 right-part">
 
-                    <div className="row">
-                        <div className="col-12">
-
+                    <div className="row w-100">
+                   
+                            <div className="col-12 logo-wrapper">
+                            <img  className="center" src={`${process.env.PUBLIC_URL}/landing_assets/footer-logo.svg`}
+                                />
+                            
                         </div>
-
+                        <div className="col-12 subtitle text-center">
+                            XRP’s Original Wallet
+                        </div>
                     </div>
                     <div className="row img-wrapper ">
 
@@ -90,7 +93,14 @@ const LoginPage = ({ classes }) => {
                     </div>
 
                 </div>
+            
+            </div>
+           <div className="row form-footer">
+                <div className="col-12">
 
+                    New User? <a href="#" >Register</a>
+
+                </div>
             </div>
         </div>
 
